875da80cb4fc90cfd46495a2065ef33609d1bd27
Ričards <ricards.porins@giraffe360.com>
2025-09-04 17:03:19 +0300

    Trying to include fusb302

--- workspace/boards/custom/powerboard/board.cmake
+++ workspace/boards/custom/powerboard/board.cmake
@@ -1,6 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "--device=atsam4s8c" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-
-include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+board_runner_args(openocd "--use-elf")
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
